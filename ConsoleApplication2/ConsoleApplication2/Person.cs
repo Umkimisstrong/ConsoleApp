f4c31c69ede8fc8b566c8d723bb6e7ee7ed89293
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-03-28 14:38:31 +0900

    Console Test

---
+++ ConsoleApplication2/ConsoleApplication2/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Person
+    {
+        private String  name;
+
+        public Person(String val)
+        {
+            name = val;
+        }
+
+        public static String get()
+        {
+            return name;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        /// <summary>
+        /// Static 변수 메소드는 Static이 아닌 변수를
+        /// 사용하게 되면 해당 변수를 사용한 구간에 오류밑줄 생긴다.
+        /// [Person 의 return name; 구문]
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Person obj = new ConsoleApplication2.Person("Kim");
+            obj.print();
+        }
+    }
+}
