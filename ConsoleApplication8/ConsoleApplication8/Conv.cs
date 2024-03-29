f857de5f2d7f18f7455b5cbfb46a211b02bab4e2
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-03-29 09:22:44 +0900

    Console Test
    Console Test

---
+++ ConsoleApplication8/ConsoleApplication8/Conv.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Conv
+    {
+        public int a;
+        public Conv(int a)
+        {
+            this.a = a;
+        }
+
+        public int func()
+        {
+            int b = 1;
+            for (int i = 0; i < a; i++)
+            {
+                b = a * i + b;
+            }
+            // if a = 5;
+            // 1회전
+            // b = 5 * 0 + 1 = 1
+
+            // 2회전
+            // b = 5 * 1 + 1 = 6
+
+            // 3회전
+            // b = 5 * 2 + 6 = 16
+
+            // 4회전
+            // b = 5 * 3 + 16 = 31
+
+            // 5회전
+            // b = 5 * 4 + 31 = 51
+
+
+            return a + b;
+
+            // a + b = 5 + 51 = 56
+        }
+    }
+}
---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conv obj = new ConsoleApplication8.Conv(3);
+
+            obj.a = 5;
+
+            int b = obj.func();
+            // 5 + 56 = 61
+            Console.WriteLine(obj.a+ b);
+
+        }
+    }
+}
