c1670b895c4844ed3b7717492dc5c5ca3607d9fe
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-04-01 08:55:28 +0900

    Update
    Update

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 2, 3 };
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(arr[i] + "");
+            }
+
+            Console.Write("End");
+        }
+
+
+    }
+}
