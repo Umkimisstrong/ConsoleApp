913ccf1502b23fbbbcb4d61f7b40694843c3ced1
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-04-02 14:46:50 +0900

    Console test

---
+++ ConsoleApplication13/ConsoleApplication13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1 == 2 ^ 1 == 1);
+            Console.WriteLine(1 == 1 ^ 1 == 2);
+            Console.WriteLine(1 == 1 ^ 1 == 1);
+            Console.WriteLine(1 == 3 ^ 1 == 4);
+
+            //Console.WriteLine(1 == 2 ^ 1 == 1);
+            Console.WriteLine("END");
+        }
+    }
+}
