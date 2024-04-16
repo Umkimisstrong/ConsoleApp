ee2cb22f2da5dc5b2fa9daa24b5b491d3e7d926b
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-04-16 09:20:40 +0900

    tests

---
+++ ConsoleApplication14/ConsoleApplication14/TestA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class TestA
+    {
+        public void paint()
+        {
+            Console.Write("A");
+            draw();
+        }
+
+        public void draw()
+        {
+            Console.Write("B");
+            while.draw();
+        }
+
+    }
+}
---
+++ ConsoleApplication14/ConsoleApplication14/TestB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class TestB : TestA
+    {
+        public new void paint()
+        {
+            base.draw();
+            Console.Write("C");
+            this.draw();
+        }
+
+        public new void draw()
+        {
+            Console.Write("D");
+        }
+
+    }
+}
